a47e9c330da207d6bf12e2bfd3795be2beb59fb4
Paulo Panaligan <panaliga@gmail.com>
2013-10-07 18:25:02 -0700

    Revert "integ [helpshift integrated]"
    This reverts commit 3fef0de2e5ba5646e09ec112357a5de1022615cf.

--- src/com/wwc/jajing/activities/MainActivity.java
+++ src/com/wwc/jajing/activities/MainActivity.java
@@ -1,9 +1,10 @@
 package com.wwc.jajing.activities;
 
 import java.io.IOException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-import com.helpshift.Helpshift;
+
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
@@ -49,7 +50,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener, Act
 	public static final String DASHBOARD_INTENT = "com.exmaple.jajingprototype.intent.DASHBOARD_NOTIFICATION_AVAILABILITY_STATUS";
 	
 	/* For Navigation Drawer */
-	private String[] navigation = new String[] { "History", "Time Settings", "My Status", "Help" };
+	private String[] navigation = new String[] { "History", "Time Settings", "My Status" };
 	private DrawerLayout mDrawerLayout;
 	private ActionBarDrawerToggle mDrawerToggle;
 	private ListView mDrawerList;
@@ -75,8 +76,6 @@ public class MainActivity extends Activity implements ActionBar.TabListener, Act
 	private IntentFilter intentFilter = new IntentFilter(
 			MainActivity.DASHBOARD_INTENT);
 
-    Helpshift hs;
-
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -84,11 +83,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener, Act
 		setContentView(R.layout.activity_drawer_main);
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle("   Welcome   ");
-
-        hs =  new Helpshift(this);
-        hs.install(this, "3bc1308ceb500d13a03d86846d14049c", "detach.helpshift.com",
-                   "detach_platform_20130816203536774-eca8fae11820a43");
-
+		
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
 		StrictMode.setThreadPolicy(policy); 
@@ -287,9 +282,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener, Act
 					MainActivity.this.setTitle("Welcome");
 				}
 				break;
-            case 3:
-                hs.showSupport(MainActivity.this);
-                break;
+
 			}
 		}
 
